47ac7e7f9c1cf71f89df509b80bf0b5bb276e6cd
Oswin Rodrigues <oswinrods@gmail.com>
2024-03-23 05:35:52 +0000

    Initialize submodule in freshly cloned repo

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/bash
 
-WORKSPACE_ROOT=/workspaces/tdd-cpp
 sudo apt-get update
+
+WORKSPACE_ROOT=/workspaces/tdd-cpp
+cd $WORKSPACE_ROOT
+git submodule init
+git submodule update
 
 # Installing vcpkg C/C++ package manager
 sudo apt-get --yes install ninja-build # vcpkg uses Ninja for its builds
@@ -9,7 +13,6 @@ export VCPKG_ROOT=$WORKSPACE_ROOT/vcpkg
 export VCPKG_FORCE_SYSTEM_BINARIES=true # Needed for ARM platforms, like M1 Macs
 unset VCPKG_DOWNLOADS # If unset, installation script uses $VCPKG_ROOT/downloads
 bash $VCPKG_ROOT/bootstrap-vcpkg.sh # Run installation script
-export PATH=$PATH:$VCPKG_ROOT # Allow vcpkg instance to be found by bash
 
 # Installing dependencies for vcpkg packages
 sudo apt-get --yes install pkg-config # Needed for GoogleTest
