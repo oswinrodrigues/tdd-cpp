cefece6cf9efd763507bcfc27679ecc7e254407f
Oswin Rodrigues <oswinrods@gmail.com>
2024-03-30 07:51:25 +0000

    Refactor: intention-revealing name

--- src/soundex_test.cpp
+++ src/soundex_test.cpp
@@ -3,7 +3,11 @@ using testing::Eq;
 
 class Soundex {
 public:
-  std::string encode(const std::string& word) const{
+  std::string encode(const std::string& word) const {
+    return zeroPad(word);
+  }
+private:
+  std::string zeroPad(const std::string& word) const {
     return word + "000";
   }
 };
