8d2a3065e0b07532af0a556ee58988e89ca94848
Oswin Rodrigues <oswinrods@gmail.com>
2024-04-02 08:01:22 +0000

    Refactor: make more declarative

--- inc/soundex.hpp
+++ inc/soundex.hpp
@@ -6,11 +6,16 @@
 class Soundex {
 public:
   std::string encode(const std::string& word) const {
-    auto encoded = word.substr(0, 1);
-    if (word.length() > 1) { encoded += "1"; }
-    return zeroPad(encoded);
+    return zeroPad(getHead(word) + getDigits(word));
   }
 private:
+  std::string getHead(const std::string& word) const {
+    return word.substr(0, 1);
+  }
+  std::string getDigits(const std::string& word) const {
+    if (word.length() > 1) { return "1"; }
+    return "";
+  }
   std::string zeroPad(const std::string& word) const {
     auto zeroCount = 4 - word.length();
     return word + std::string(zeroCount, '0');
